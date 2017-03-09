e79ae2126a5ec6cf46dd13ad3ee73224c5dc1b70
NIKITA TANK <nikitatank1212@csu.fullerton.edu>
2017-03-09 13:15:18 -0800

    added folders

--- LazyReader.py
+++ LazyReader.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template,redirect,url_for,request
+import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+def index():
+    #return redirect(url_for('index.html'))
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.getenv('PORT', 5000))
+   # app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=False, port=port, host='127.0.0.1')
